b96724962f0fb9c3e39522c5ae4e37c523130b68
LiamNussie <liamace007@gmail.com>
2022-05-28 13:52:43 +0100

    added cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors")
 const { Timestamp } = require("mongodb");
 const mongoose = require("mongoose");
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
